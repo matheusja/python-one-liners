df81e2c7334edac7f5d42aa202cbe12ac4192125
Matheus <matheusjahnke@hotmail.com>
2021-01-04 20:46:18 -0300

    Few fixes and added explanations

--- iterator.py
+++ iterator.py
@@ -1,3 +1,8 @@
-import itertools
+import itertools import *
 
-sliding_window = lambda n, l : zip(*(itertools.islice(l, i, None) for i in range(n)))
+# sliding_window([a0, a1, ..., an], k) = [(a0, a1, ..., ak), (a1, a2, a3, ... a(k+1)), ..., (a(n-k+1), a(n-k+2), ... an)]
+
+# Works for list, everything else might have problems due to competing iterators
+sliding_window = lambda n, l : zip(*(islice(l, i, None) for i in range(n)))
+# This version solves that by using tee
+sliding_window_tee = lambda itbl, n : zip(*(islice(itblc, i, None) for i, itblc in enumerate(tee(itbl, n))))
--- vectorial.py
+++ vectorial.py
@@ -1,9 +1,16 @@
-# Recebe 2 listas e retorna uma que é o resultado da diferença ponto a ponto entre as duas 
-import operator
+import operator import *
 
-scalar_mult = lambda l1, l2 : sum(map(operator.mul, l1, l2))
+# scalar_mult([a0, a1, a2, ..., an], [b0, b1, b2, ..., bm]) = a0 * b0  +  a1 * b1  +  a2 * b2  +  ...  + an * bn
+# trims the iterable with more values
+scalar_mult = lambda l1, l2 : sum(map(mul, l1, l2))
 
-map_list_of_lists = lambda f, *mats : list(map((lambda *linhas : list(map(f, *linhas))), *mats))
+
+# apply function to list of lists
+map_list_of_lists = lambda f, *mats : list(map((lambda *lines : list(map(f, *lines))), *mats))
+# multiply every element of a iterable by a "scalar" value 'k'
 mult_list_by_escalar = lambda k, l : map((lambda x : x * k), l)
-matricial_add = lambda l1, l2 : map(operator.add, l1, l2)
-matricial_mult = lambda mat, x : list(map((lambda l : mult_pnt_a_pnt(l, x)), mat))
+# add, value by value, two iterable 
+# trims the iterable with more values
+matricial_add = lambda l1, l2 : map(add, l1, l2)
+# matricial multiplication
+matricial_mult = lambda mat, x : list(map((lambda l : scalar_mult(l, x)), mat))
